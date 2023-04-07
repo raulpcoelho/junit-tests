4d5885017829bea0521814beddff1a22bc55a156
Raul Coelho <raulpereira5@gmail.com>
2023-04-07 01:25:13 -0300

    first commit

---
+++ src/main/java/com/examples/tests/App.java
@@ -0,0 +1,14 @@
+package com.examples.tests;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static int return2() {
+        return 2;
+    }
+    public static void main(String[] args) {
+        System.out.println(return2());
+    }
+}
---
+++ src/test/java/com/examples/tests/AppTest.java
@@ -0,0 +1,18 @@
+package com.examples.tests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testReturn2() {
+        assertEquals(2, App.return2());
+    }
+}
